af63e3f09cc2ca8ce16bf8d27e061961f28f6521
Sir Eric <echeungsk@gmail.com>
2020-03-08 14:51:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class Application
   
-  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+  @@items = [Item.new("Yogurt", 0.99), Item.new("Eggs", 1.49)]
   
   def call(env)
     resp = Rack::Response.new
